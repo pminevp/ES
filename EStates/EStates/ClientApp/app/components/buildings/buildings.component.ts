5a39aa2a20acedf09ce2792cf533ac688feb76db
pminev <pminev@hotmail.com>
2017-07-22 16:11:11 +0300

    add new Building functionallity UI

--- EStates/EStates/ClientApp/app/components/buildings/buildings.component.ts
+++ EStates/EStates/ClientApp/app/components/buildings/buildings.component.ts
@@ -8,5 +8,7 @@ import { Component } from "@angular/core";
 })
 
 export class BuildingsComponent {
-
+    building1 = require("../../assets/images/Buildings/GotseDelchev-214.jpg");
+    building2 = require("../../assets/images/Buildings/sgrada2.jpg");
+    building3 = require("../../assets/images/Buildings/sgrada3.jpg");
 }
