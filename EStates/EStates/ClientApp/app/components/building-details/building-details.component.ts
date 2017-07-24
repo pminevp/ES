68442ea14521cabdda310153dda72ab60b1ff88e
Petar M <pminev@hotmail.com>
2017-07-24 09:32:38 +0300

    Buildign details phase 1

---
+++ EStates/EStates/ClientApp/app/components/building-details/building-details.component.ts
@@ -0,0 +1,27 @@
+﻿import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { BuildingsEndpointService } from "../../services/buildings-endpoint.service";
+import { Building } from "../../models/building";
+
+@Component({
+    selector: 'app-buildingDetails',
+    templateUrl: './building-details.component.html',
+    styleUrls: ['./building-details.component.css']
+})
+
+export class BuildingDetailsComponent {
+
+    private selectedBuildingId: number;
+    private selectedBuilding: Building;
+
+    constructor(route: ActivatedRoute, private buildingsEndpoint: BuildingsEndpointService)
+    {
+        var id = route.snapshot.params['id'];
+        if (id === undefined) {           
+        }
+        else {
+            this.selectedBuildingId = id;
+            this.selectedBuilding = buildingsEndpoint.GetSelectedBuilding(id);
+        }
+    }
+}
---
+++ EStates/EStates/ClientApp/app/services/buildings-endpoint.service.ts
@@ -0,0 +1,30 @@
+﻿import { Injectable } from "@angular/core";
+import { Building } from "../models/building";
+
+@Injectable()
+export class BuildingsEndpointService
+{
+
+    public GetAllBuildings() {
+        return this.GetStoredBuildings();
+    }
+
+    public GetSelectedBuilding(id:number)
+    {
+        var building = this.GetStoredBuildings().find(x => x.id === id);
+        return building;
+    }
+
+
+    private GetStoredBuildings()
+    {
+        var buildings = new Array<Building>();
+        buildings.push(
+            { image: "../../assets/images/Buildings/GotseDelchev-214.jpg", name: 'Сграда 1', description: 'малка сграда в центъра на софия', id: 0 },
+            { image: "../../assets/images/Buildings/sgrada2.jpg", name: 'Сграда 2', description: 'блок 214 гоце делчев', id: 1 },
+            { image: "../../assets/images/Buildings/sgrada3.jpg", name: 'К-с Фонаните', description: 'Комплексът без край', id: 2 }
+        );
+
+        return buildings;
+    }
+}
